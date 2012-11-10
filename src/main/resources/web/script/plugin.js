4a20e4bc1e169b3d8285918e181d284847c873be
dgf <github@dagnu.de>
2012-11-10 12:45:23 +0100

    reuse absolute paths

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -1,5 +1,5 @@
 // configure CKEditor image upload and browsing
 dm4c.add_plugin('de.deepamehta.images', function () {
-    CKEDITOR.config.filebrowserImageBrowseUrl = 'de.deepamehta.images/browse.html'
-    CKEDITOR.config.filebrowserImageUploadUrl = 'images/upload'
+    CKEDITOR.config.filebrowserImageBrowseUrl = '/de.deepamehta.images/browse.html'
+    CKEDITOR.config.filebrowserImageUploadUrl = '/images/upload'
 })
