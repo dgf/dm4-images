0cc580fb0fed757571c81ddb2075d143152eb4d7
Malte Reißig <malte@deepamehta.org>
2013-03-04 17:47:06 +0100

    adapted to be used with ck-editor 4.0.1.1. + outside dm4-webclient

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -2,4 +2,8 @@
 dm4c.add_plugin('de.deepamehta.images', function () {
     CKEDITOR.config.filebrowserImageBrowseUrl = '/de.deepamehta.images/browse.html'
     CKEDITOR.config.filebrowserImageUploadUrl = '/images/upload'
+    // Note: the following config is new to ckeditor as of 4.0.1.1 (in case you want to upgrade) and
+    // appears in the "Bild-Info"-Tab, commonly utilized to select an already uploaded image
+    // config.filebrowserImageBrowseUrl = '/de.deepamehta.images/browse.html'
+    // Hint: image-upload features in CKEDITOR do neither work with "inline"-editors nor with a "Basic"-distribution
 })
