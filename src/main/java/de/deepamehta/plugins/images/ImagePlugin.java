59a22eb4d2d934e17de3476a65d0bc2039175e15
Malte Reißig <malte@mikromedia.de>
2016-06-14 18:54:16 +0200

    Added basic 300px image resize command using imgscalr.

--- src/main/java/de/deepamehta/plugins/images/ImagePlugin.java
+++ src/main/java/de/deepamehta/plugins/images/ImagePlugin.java
@@ -1,17 +1,24 @@
 package de.deepamehta.plugins.images;
 
+import de.deepamehta.core.Topic;
 import de.deepamehta.core.osgi.PluginActivator;
 import de.deepamehta.core.service.Inject;
 import de.deepamehta.core.service.Transactional;
 import de.deepamehta.files.DirectoryListing.FileItem;
 import de.deepamehta.files.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImagingOpException;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+import org.imgscalr.Scalr;
 
 /**
  * CKEditor compatible resources for image upload and browse.
@@ -52,6 +59,52 @@ public class ImagePlugin extends PluginActivator {
         } catch (Exception e) {
             log.severe(e.getMessage() + ", caused by " + e.getCause().getMessage());
             return getCkEditorCall(func, "", e.getMessage());
+        }
+    }
+
+    @GET
+    @Path("/resize/{topicId}/{maxSize}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Topic resizeImageFileTopic(@PathParam("topicId") long fileTopicId, @PathParam("maxSize") int maxSize) {
+        log.info("Resize Image File (Topic ID: " + fileTopicId + ") Max Size: " + maxSize + "px");
+        try {
+            File imageFile = fileService.getFile(fileTopicId);
+            Topic imageFileTopic = dm4.getTopic(fileTopicId).loadChildTopics();
+            String imageFileTopicRepositoryPath = imageFileTopic.getChildTopics().getString("dm4.files.path");
+            log.info("Image File Topic Path requested to be RESIZED: " + imageFileTopicRepositoryPath);
+            BufferedImage srcImage = ImageIO.read(imageFile); // Load image
+            log.info("Image File Buffered " + srcImage);
+            BufferedImage scaledImage = Scalr.resize(srcImage, maxSize); // Scale image
+            String imageFileName = imageFile.getName();
+            String imageFileEnding = imageFileName.substring(imageFileName.indexOf(".") + 1);
+            String imageFileBeginning = imageFileName.substring(imageFileName.lastIndexOf("/") + 1, imageFileName.indexOf("."));
+            String imageFileTopicParentRepositoryPath = imageFileTopicRepositoryPath.substring(0, imageFileTopicRepositoryPath.lastIndexOf("/"));
+            log.info("Image File Name Ending " + imageFileEnding);
+            String newFileName = imageFileBeginning + "-300px." + imageFileEnding;
+            File resizedImageFile = new File(imageFile.getParent() + File.separator + newFileName);
+            if (resizedImageFile.createNewFile()) {
+                log.info("Created New Image File " + resizedImageFile.getAbsolutePath());
+                if (imageFileEnding.equals("jpg")) imageFileEnding = "jpeg";
+                ImageIO.write(scaledImage, imageFileEnding, resizedImageFile);
+                log.info("Wrote new Image File... " + resizedImageFile.getAbsolutePath());
+            } else {
+                log.warning("Image File already exists" + imageFile.getParent() + File.separator + newFileName);
+                if (imageFileEnding.equals("jpg")) imageFileEnding = "jpeg";
+                ImageIO.write(scaledImage, imageFileEnding, resizedImageFile);
+                log.info("Updated Image File... " + resizedImageFile.getAbsolutePath());
+            }
+            log.info("Fetching Image File Topic " + resizedImageFile.getAbsolutePath());
+            return fileService.getFileTopic(imageFileTopicParentRepositoryPath + File.separator + newFileName);
+        } catch (IOException e) {
+            log.severe("Resizing Image Failed IOException, " + e.getMessage() + ", caused by " + e.getCause());
+            return null;
+        } catch (IllegalArgumentException e) {
+            log.severe("Resizing Image Failed IllegalArgumentException, " + e.getMessage() + ", caused by " + e.getCause());
+            return null;
+        } catch (ImagingOpException e) {
+            log.severe("Resizing Image Failed ImagingOpException " + e.getMessage() + ", caused by " + e.getCause());
+            return null;
         }
     }
 
--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -3,4 +3,26 @@ dm4c.add_plugin('de.deepamehta.images', function () {
     // config lines are the same / work with "image2" plugin as well
     CKEDITOR.config.filebrowserImageBrowseUrl = '/de.deepamehta.images/browse.html'
     CKEDITOR.config.filebrowserImageUploadUrl = '/images/upload'
+
+    function doResize() {
+        var imageTopic = dm4c.restc.request('GET', '/images/resize/' + dm4c.selected_object.id + "/300")
+        dm4c.show_topic(new Topic(imageTopic), "show", undefined, true)
+    }
+
+    dm4c.add_listener('topic_commands', function (topic) {
+        // Note: create permission now managed by core
+        var commands = []
+        if (topic.type_uri === 'dm4.files.file' && dm4c.restc.get_username()) {
+            if (topic.childs["dm4.files.media_type"].value === "image/jpeg" ||
+                topic.childs["dm4.files.media_type"].value === "image/png")
+            commands.push({is_separator: true, context: 'context-menu'})
+            commands.push({
+                label: '300px Resize',
+                handler: doResize,
+                context: ['context-menu', 'detail-panel-show']
+            })
+        }
+        return commands
+    })
+
 })
